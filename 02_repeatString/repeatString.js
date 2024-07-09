120a3c98e24cdca72189c32ff2c01fcf0aac58c5
zwhahn <117476820+zwhahn@users.noreply.github.com>
2024-07-09 17:02:51 -0400

    Complete 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(word, repitionQuantity) {
+    let output = ''
+    if (repitionQuantity >= 0){
+        for (;repitionQuantity > 0; repitionQuantity--){
+            output = output.concat(word)
+        }
+        return output
+    }else {
+        return 'ERROR'
+    }
 };
 
 // Do not edit below this line
