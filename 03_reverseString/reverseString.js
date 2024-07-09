95f0ebafb3af4c34dc68fd82200e52aa4ee18542
zwhahn <117476820+zwhahn@users.noreply.github.com>
2024-07-09 18:32:45 -0400

    Complete 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(randomString) {
+    let stringLength = randomString.length;
+    const charArray = [];
+    let output = '';
+    for (; stringLength > 0; stringLength--) {
+        character = randomString[stringLength-1]
+        charArray.push(character)
+    } for (let char of charArray) {
+        output = output.concat(char)
+    } return output
 };
 
 // Do not edit below this line
