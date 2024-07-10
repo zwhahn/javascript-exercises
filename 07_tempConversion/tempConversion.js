b6cae9e91fd7d995752739d3ee0ff1e4d8df27a7
zwhahn <117476820+zwhahn@users.noreply.github.com>
2024-07-10 17:22:52 -0400

    Complete 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  return Math.round((tempF-32)*(5/9)*10)/10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  return Math.round(((tempC*(9/5))+32)*10)/10
 };
 
 // Do not edit below this line
