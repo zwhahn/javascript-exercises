cf1f9070402ae672a6e178148d71bc0c99c347df
zwhahn <117476820+zwhahn@users.noreply.github.com>
2024-07-10 17:15:58 -0400

    Complete 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,14 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if (year%4 === 0 && year%400 === 0){
+        return true
+    }
+    else if (year%4 === 0 && year%100 === 0){
+        return false
+    }
+    else if (year%4 === 0) {
+        return true
+    }
+    return false
 
 };
 
