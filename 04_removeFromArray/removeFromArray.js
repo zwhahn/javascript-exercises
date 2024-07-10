ff0d7f7847cdd9226614747d9aafc3dbd9d742cf
zwhahn <117476820+zwhahn@users.noreply.github.com>
2024-07-10 16:44:11 -0400

    Complete 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArray, ...valuesToRemove) {
+    let outputArray = [];
+    // let lengthOfValuesToRemove = valuesToRemove.length;
+    for (inputValue of inputArray) {
+        if (valuesToRemove.includes(inputValue) === false) {
+            outputArray.push(inputValue);
+        }
+    }
+    return outputArray;
 };
 
 // Do not edit below this line
