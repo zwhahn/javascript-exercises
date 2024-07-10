ae058411b94db381367177b7dde5e8b28abcef0f
zwhahn <117476820+zwhahn@users.noreply.github.com>
2024-07-10 17:10:11 -0400

    Complete 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(firstValue, secondValue) {
+    let sum = 0;
+    if (firstValue < 0 || secondValue < 0) {
+        return 'ERROR'
+    } else if (typeof(firstValue) != 'number' || typeof(secondValue) != 'number'){
+        return 'ERROR'
+    } else if (firstValue < secondValue) {
+        var biggerValue = secondValue
+        var smallerValue = firstValue
+    } else if (firstValue > secondValue) {
+        var biggerValue = firstValue
+        var smallerValue = secondValue
+    } 
+    for (i = smallerValue; i <= biggerValue; i++){
+        sum = sum + i;
+    }
+    return sum 
 };
 
 // Do not edit below this line
