85f99c9261d86d7b0d707609f14d1f2a4e778c00
zwhahn <117476820+zwhahn@users.noreply.github.com>
2024-07-20 07:24:37 -0400

    Complete 08_factorial

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((accumulator, current) => 
+    accumulator + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((accumulator, current) => 
+    accumulator * current, 1);
 };
 
-const power = function() {
-	
+const power = function(base, power) {
+  return base ** power;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let sum = 1;
+  if (num === 0){
+    return sum = 1;
+  }
+  for (i = num; i > 0; i--){
+    sum = sum * i;
+  }
+  return sum
 };
 
 // Do not edit below this line
