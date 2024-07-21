f07330e77353a17fb603a27b132010357084281e
zwhahn <117476820+zwhahn@users.noreply.github.com>
2024-07-20 20:35:33 -0700

    complete 11_getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let bookTitles = []
+    books.forEach((item) => bookTitles.push(item.title));
+    return bookTitles;
 };
 
 // Do not edit below this line
