16bf8c35f2cb5e3924debbc69fb56a1b4422095d
zwhahn <117476820+zwhahn@users.noreply.github.com>
2024-07-20 20:01:04 -0700

    complete 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
-
+const palindromes = function (inputString) {
+    spaceless = inputString.split(' ').join('');
+    punctuationless = spaceless.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+    noCaps = punctuationless.toLowerCase();
+    let stringArray = noCaps.split('');
+    console.log(`stringArray: ${stringArray}`)
+    reverseArray = stringArray.reverse();
+    console.log(`reverseArray: ${reverseArray}`)
+    reverseString = reverseArray.join('')
+    console.log(`reverseString: ${reverseString}`)
+    if (reverseString === noCaps){
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
