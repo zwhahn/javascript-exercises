8b8c39b445b283ee2edfd1ebb7cb624aed9d16d2
zwhahn <117476820+zwhahn@users.noreply.github.com>
2024-07-20 20:18:55 -0700

    complete 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let fibArray = [1, 1];
+    if (num == 0){
+        return 0
+    }
+    if (num < 0){
+        return "OOPS"
+    }
+    for (i=2; i < num; i++){
+        // console.log(`fibArray[i-1] ${fibArray[i-1]}`);
+        let nextVal = fibArray[i-2] + fibArray[i-1];
+        fibArray.push(nextVal);
+    }
+    // console.log(`fibArray: ${fibArray}`)
+    return fibArray[num-1]
 };
 
 // Do not edit below this line
