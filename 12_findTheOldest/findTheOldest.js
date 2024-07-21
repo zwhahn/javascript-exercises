0b9d85126cd280efe84b7a901b0297ac759dbaf1
zwhahn <117476820+zwhahn@users.noreply.github.com>
2024-07-20 21:05:29 -0700

    complete 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    ageArray = []
+    people.forEach((person) => {const hasDied = person.hasOwnProperty('yearOfDeath');
+    if(!hasDied){
+        var thisYear = new Date().getFullYear(0)
+        person.age = (thisYear - person.yearOfBirth);
+    } else {
+        person.age = (person.yearOfDeath - person.yearOfBirth)
+    }
+    });
+    console.log(`people unsorted: ${JSON.stringify(people)}`)
+    people.sort((a,b) => b.age - a.age)
+    console.log(`people sorted: ${JSON.stringify(people)}`)
+    return people[0]
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
